b1393ecbd2c5c8f0334c06e1ad9c1be562082fd1
Rudy <30980904+SyncTank@users.noreply.github.com>
2023-11-15 07:50:19 -0500

    SetUp

--- src/test/java/com/syncTank/spellCountPluginTest.java
+++ src/test/java/com/syncTank/spellCountPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.syncTank;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class spellCountPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(spellCountPlugin.class);
 		RuneLite.main(args);
 	}
 }
